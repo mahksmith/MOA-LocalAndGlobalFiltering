296b9c91f277dc82c71f565d13d8d833840a952e
Mark Smith <kybernz@gmail.com>
2011-09-28 13:44:26 +1300

    Added instance grouping into chunks: Instances.java is found inside the weka API, not MOA.

--- moa/streams/filters/LocalGlobalFiltering.java
+++ moa/streams/filters/LocalGlobalFiltering.java
@@ -5,23 +5,24 @@
 package moa.streams.filters;
 
 import moa.core.InstancesHeader;
+import moa.core.ObjectRepository;
 import moa.options.FloatOption;
 import moa.options.IntOption;
+import moa.tasks.TaskMonitor;
 import weka.core.Instance;
+import weka.core.Instances;
 
 public class LocalGlobalFiltering extends AbstractStreamFilter {
 
 	@Override
 	public InstancesHeader getHeader() {
-		// TODO Auto-generated method stub
-		return null;
+		return new InstancesHeader(this.instances);
 	}
 	
 	@Override
 	public String getPurposeString() {
 		return "Accurately identify and remove mislabeled data for more accurate models.";
 	}
-
 	
 	/* The options after this are just a guess to the default value. Perhaps some quick 
 	 * research could be done into good default values? 
@@ -29,7 +30,7 @@ public class LocalGlobalFiltering extends AbstractStreamFilter {
 	public IntOption chunkSizeOption = new IntOption("chunkSize", 'N', "Set the size of each chunk.", 20);
 	
 	public FloatOption removalAlphaOption = new FloatOption("removalAlpha", 'a', 
-			"The percentage of instances to remove from the stream.", 0.05, 0.0, 1.0);
+			"The percentage of instances to remove from the stream.", 0.1, 0.0, 1.0);
 	
 	public IntOption nClassifiersGlobalFilteringOption = new IntOption("nClassifiersGlobal", 'k', 
 			"Number of classifiers involved in Global Filtering", 20);
@@ -40,12 +41,42 @@ public class LocalGlobalFiltering extends AbstractStreamFilter {
 	public IntOption nHeterogeneousClassifiersLocalOption = new IntOption("nHeterogeneousClassifiers", 'g',
 			"Number of heterogeoneous classifiers built for local filtering.", 10);
 
-	// TODO: maybe later we could add some noise to this for debugging purposes.
+	private Instances instances;
+	
+	protected int chunkSize;
+	protected float removalAlpha;
+	protected int nGlobalClassifiers;
+	protected int nLocalFolds;
+	protected int nHeterogeneousClassifiers;
+	
+	@Override
+	public void	prepareForUseImpl (TaskMonitor monitor, ObjectRepository repository) {
+		this.chunkSize 					= chunkSizeOption.getValue();
+		this.removalAlpha 				= (float) removalAlphaOption.getValue();	// Perhaps a bug???
+		this.nGlobalClassifiers 		= nClassifiersGlobalFilteringOption.getValue();
+		this.nLocalFolds 				= nFoldsLocalFilterOption.getValue();
+		this.nHeterogeneousClassifiers 	= nHeterogeneousClassifiersLocalOption.getValue();
+		
+		super.prepareForUse(monitor, repository);
+	}
 	
 	@Override
 	public Instance nextInstance() {
-		// TODO Auto-generated method stub
+		
+		if (instances == null) {
+			this.instances = new Instances(instances, chunkSize);
+		}
+		
+		if (this.instances.size() < chunkSize) {
+			this.instances.add(instance)
+		}
+		
 		return null;
+	}
+
+	private void processChunk(Instances instances2) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
@@ -57,7 +88,6 @@ public class LocalGlobalFiltering extends AbstractStreamFilter {
 	@Override
 	protected void restartImpl() {
 		// TODO Auto-generated method stub
-
 	}
 
 }
